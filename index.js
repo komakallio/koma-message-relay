3c1230c224032940466f7db4b059271120d0be86
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-02-10 20:19:02 +0200

    Forward messages to ZeroMQ

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 /* eslint-disable no-console */
 const redis = require('redis');
+const zmq = require('zeromq');
+const config = require('./config');
 
-const redisClient = redis.createClient();
+const redisSubscriber = redis.createClient();
+redisSubscriber.config('set', 'notify-keyspace-events', 'Ez');
+redisSubscriber.on('error', err => console.log(`${err}`));
 
-redisClient.config('set', 'notify-keyspace-events', 'KEA');
+const pubSocket = zmq.socket('pub');
+pubSocket.connect(config.BROKER_ADDRESS);
 
-redisClient.on('error', err => console.log(`${err}`));
+const redisClient = redisSubscriber.duplicate();
+const messageHandler = (_, topic) => {
+  redisClient.zrevrange(topic, 0, 0, 'withscores', (err, response) => {
+    if (err) throw err;
+    const [value, score] = response;
+    const message = `${topic} ${score} ${value}`;
+    pubSocket.send(message);
+  });
+};
 
-redisClient.psubscribe(['__keyevent@0__:*', '__keyspace@0__:*']);
-redisClient
-  .on('pmessage', (pattern, channel, message) => console.log(`${channel}: ${message}, matching pattern ${pattern}`));
+redisSubscriber.subscribe('__keyevent@0__:zadd');
+redisSubscriber.on('message', messageHandler);
+
+/* Test message sender */
+const adderClient = redisSubscriber.duplicate();
+const sendMessages = () => {
+  adderClient.zadd('ptu', Date.now(), '{"data": "this is fake news"}');
+  setTimeout(sendMessages, 2000);
+};
+
+setTimeout(sendMessages, 2000);
